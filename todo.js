418b70f98a84da11f6c6a4c4b6feb783bd4bb0ce
mariana-im <marianaislasm2004@gmail.com>
2025-01-20 12:32:06 -0700

    Issues with Event Listeners and Scrolling

--- todo.js
+++ todo.js
@@ -9,16 +9,14 @@ function addTask() {
         const NewBox = document.createElement('div')
         const txt = document.createElement('a')
         const img1 = document.createElement('div')
-        const img2 = document.createElement('div')
         
         img1.setAttribute('class', 'circle')
-        img2.setAttribute('id', 'x')
+        txt.setAttribute('class', 'txt')
         txt.textContent = Input.value
         NewBox.setAttribute('class', 'taskbox')
-        NewBox.setAttribute('id', 'task-box')
 
         document.getElementById("tasks-box").append(NewBox)
-        NewBox.append(img1,txt,img2)
+        NewBox.append(img1,txt)
 
         Input.value = "";
     }
@@ -26,26 +24,19 @@ function addTask() {
 
 //Event listeners
 
-document.addEventListener("click", e => {
-    if (e.target.matches(".taskbox")) {
-        taskbox = document.querySelector(".taskbox")
-        e.target.classList.toggle("click")
-    }
-})
-
 
 document.addEventListener("mouseover", e => {
-    const x = document.getElementById("x")
-    if (e.target.matches(".task-box")) {
-        x.style.visibility = "visible"
+    if  (e.target.matches(".taskbox")) {
+        const xicon = document.createElement('img')
+        xicon.src = 'x-icon.png'
+        xicon.setAttribute('class','x')
+        e.target.append(xicon)
     }
 })
 
-document.addEventListener("click", e => {
-    const x = document.getElementById("x")
-    const ToDoBox = document.getElementById("task-box");
-    if (e.target === x) {
-        e.target.remove();
+document.addEventListener("mouseout", e => {
+    if (e.target.matches(".taskbox")) {
+        e.target.removeChild(e.target.lastElementChild)
     }
 })
 
@@ -55,3 +46,8 @@ document.addEventListener("click", e => {
     }
 })
 
+document.addEventListener('click', e => {
+    if (e.target.matches(".x")){
+        e.target.parentNode.remove()
+    }
+})
